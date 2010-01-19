41d81b445a0be1a3c3eab338cc38a1596bd0b27c
Martin Isenburg <isenburg@cs.unc.edu>
2010-01-19 00:34:52 -0500

    added the licensing info again

--- src/mydefs.h
+++ src/mydefs.h
@@ -1,3 +1,21 @@
+/******************************************************************************
+ *
+ * Project:  laszip - http://liblas.org -
+ * Purpose:
+ * Author:   Martin Isenburg
+ *           isenburg at cs.unc.edu
+ *
+ ******************************************************************************
+ * Copyright (c) 2009, Martin Isenburg
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Licence as published
+ * by the Free Software Foundation.
+ *
+ * See the COPYING file for more information.
+ *
+ ****************************************************************************/
+
 #ifndef MYDEFS_H
 #define MYDEFS_H
 
