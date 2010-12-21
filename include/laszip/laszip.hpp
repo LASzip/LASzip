9e0515575e0a601f05f0b5554ddb785bed4c64b6
isenburg <none@none>
2010-12-21 12:20:28 -0800

    added LASzip struct to be used as VLR data

--- include/laszip/laszip.hpp
+++ include/laszip/laszip.hpp
@@ -49,6 +49,9 @@
 
 #if defined(_MSC_VER) && (_MSC_VER < 1300)
 #define LZ_WIN32_VC6
+typedef __int64   SIGNED_INT64;
+#else
+typedef long long SIGNED_INT64;
 #endif
 
 #define LASZIP_VERSION_MAJOR    1
@@ -56,14 +59,16 @@
 #define LASZIP_VERSION_REVISION 0
 
 #define LASZIP_COMPRESSION_NONE       0
-#define LASZIP_COMPRESSION_RANGE      1
-#define LASZIP_COMPRESSION_ARITHMETIC 2
+#define LASZIP_COMPRESSION_ARITHMETIC 1
+#define LASZIP_COMPRESSION_RANGE      2
+
+#define LASZIP_COMPRESSION_DEFAULT    1
 
 class LASitem
 {
 public:
 
-  enum Type { BYTE = 0, SHORT, INT, LONG, FLOAT, DOUBLE, POINT10, GPSTIME, RGB, WAVEPACKET } type;
+  enum Type { BYTE = 0, SHORT, INT, LONG, FLOAT, DOUBLE, POINT10, GPSTIME11, RGB12, WAVEPACKET13 } type;
   unsigned short size;
   unsigned short version;
 
@@ -76,18 +81,18 @@ public:
         size = 20;
         version = 0;
         break;
-    case LASitem::GPSTIME:
-        type = LASitem::GPSTIME;
+    case LASitem::GPSTIME11:
+        type = LASitem::GPSTIME11;
         size = 8;
         version = 0;
         break;
-    case LASitem::RGB:
-        type = LASitem::RGB;
+    case LASitem::RGB12:
+        type = LASitem::RGB12;
         size = 6;
         version = 0;
         break;
-    case LASitem::WAVEPACKET:
-        type = LASitem::WAVEPACKET;
+    case LASitem::WAVEPACKET13:
+        type = LASitem::WAVEPACKET13;
         size = 29;
         version = 0;
         break;
@@ -110,13 +115,13 @@ public:
     case POINT10:
       if (size != 20) return false;
       break;
-    case GPSTIME:
+    case GPSTIME11:
       if (size != 8) return false;
       break;
-    case RGB:
+    case RGB12:
       if (size != 6) return false;
       break;
-    case WAVEPACKET:
+    case WAVEPACKET13:
       if (size != 29) return false;
       break;
     case BYTE:
@@ -133,9 +138,9 @@ public:
     switch (type)
     {
     case POINT10:
-    case GPSTIME:
-    case RGB:
-    case WAVEPACKET:
+    case GPSTIME11:
+    case RGB12:
+    case WAVEPACKET13:
     case BYTE:
       return true;
       break;
@@ -152,13 +157,13 @@ public:
     case POINT10:
       if (size != 20) return false;
       break;
-    case GPSTIME:
+    case GPSTIME11:
       if (size != 8) return false;
       break;
-    case RGB:
+    case RGB12:
       if (size != 6) return false;
       break;
-    case WAVEPACKET:
+    case WAVEPACKET13:
       if (size != 29) return false;
       break;
     case BYTE:
@@ -177,13 +182,13 @@ public:
     case POINT10:
       if (version > 1) return false;
       break;
-    case GPSTIME:
+    case GPSTIME11:
       if (version > 1) return false;
       break;
-    case RGB:
+    case RGB12:
       if (version > 1) return false;
       break;
-    case WAVEPACKET:
+    case WAVEPACKET13:
       if (version > 1) return false;
       break;
     case BYTE:
@@ -203,15 +208,15 @@ public:
       if (size != 20) return false;
       if (version > 1) return false;
       break;
-    case GPSTIME:
+    case GPSTIME11:
       if (size != 8) return false;
       if (version > 1) return false;
       break;
-    case RGB:
+    case RGB12:
       if (size != 6) return false;
       if (version > 1) return false;
       break;
-    case WAVEPACKET:
+    case WAVEPACKET13:
       if (size != 29) return false;
       if (version > 1) return false;
       break;
@@ -223,6 +228,40 @@ public:
       return false;
     }
     return true;
+  }
+};
+
+class LASzip
+{
+public:
+  unsigned int compression;
+  unsigned char version_major;
+  unsigned char version_minor;
+  unsigned short version_revision;
+  unsigned int options;
+  unsigned int num_items;
+  unsigned int num_chunks;  /* not used yet  ...               */ 
+  SIGNED_INT64 num_points;  /* not mandatory ... -1 if unknown */
+  SIGNED_INT64 num_bytes;   /* not mandatory ... -1 if unknown */
+  LASitem* items;
+
+  LASzip()
+  {
+    compression = 0;
+    version_major = 1;
+    version_minor = 0;
+    version_revision = 0;
+    options = 0;
+    num_items = 0;
+    num_chunks = 1;
+    num_points = -1;
+    num_bytes = -1;
+    items = 0;
+  }
+
+  ~LASzip()
+  {
+    if (items) delete [] items;
   }
 };
 
