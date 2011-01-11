be1541535d1b52001fe43f283cbfa19dc46987c7
Michael P. Gerlek <mpg@flaxen.com>
2011-01-11 12:45:20 -0800

    "bool" is valid post-VC6

--- src/mydefs.hpp
+++ src/mydefs.hpp
@@ -38,7 +38,7 @@ typedef long long          I64;
 typedef float              F32;
 typedef double             F64;
 
-#if defined(_WIN32)
+#if defined(_MSC_VER) && (_MSC_VER < 1300)
 typedef int                BOOL;
 #else
 typedef bool               BOOL;
