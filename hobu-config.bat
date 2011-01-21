51ebaa7cc7d276a4e09a6b88dcba2a9cac3bffa8
Howard Butler <hobu.inc@gmail.com>
2011-01-21 13:29:25 -0600

    static build option

--- hobu-config.bat
+++ hobu-config.bat
@@ -3,8 +3,6 @@
 set G="NMake Makefiles"
 REM set G="Visual Studio 9 2008"
 REM set G="Visual Studio 10"
-REM set G="Visual Studio 9 2008"
-set LASZIP=D:\laszip
 REM set BUILD_TYPE=RelWithDebInfo
 REM set BUILD_TYPE=Debug
 set BUILD_TYPE=Release
@@ -12,4 +10,5 @@ set BUILD_TYPE=Release
 cmake -G %G% ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
-    %LASZIP%
+    -DBUILD_STATIC=FALSE ^
+    .
