06faa00cbcdaabc6bf021bc00a2fde36f3d1d3aa
Michael P. Gerlek <mpg@flaxen.com>
2011-10-27 10:52:17 -0700

    win32 dll fix

--- include/laszip/laszip.hpp
+++ include/laszip/laszip.hpp
@@ -73,7 +73,7 @@ typedef long long SIGNED_INT64;
 
 #include "laszipexport.hpp"
 
-class LASitem
+class LASZIP_DLL LASitem
 {
 public:
   enum Type { BYTE = 0, SHORT, INT, LONG, FLOAT, DOUBLE, POINT10, GPSTIME11, RGB12, WAVEPACKET13 } type;
