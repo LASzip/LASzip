4360e7b26ee7730c3fa0c3c99234170b0d1d59a1
Martin Isenburg <martin.isenburg@gmail.com>
2012-01-03 11:23:40 -0800

    fix seeking without chunk table + LAS 1.4 stubs

--- src/laswriteitemraw.hpp
+++ src/laswriteitemraw.hpp
@@ -157,4 +157,93 @@ private:
   U32 number;
 };
 
+class LAStempWritePoint10
+{
+public:
+  I32 x;
+  I32 y;
+  I32 z;
+  U16 intensity;
+  U8 return_number : 3;
+  U8 number_of_returns_of_given_pulse : 3;
+  U8 scan_direction_flag : 1;
+  U8 edge_of_flight_line : 1;
+  U8 classification;
+  I8 scan_angle_rank;
+  U8 user_data;
+  U16 point_source_ID;
+};
+
+class LAStempWritePoint14
+{
+public:
+  I32 x;
+  I32 y;
+  I32 z;
+  U16 intensity;
+  U8 return_number : 4;
+  U8 number_of_returns_of_given_pulse : 4;
+  U8 classification_flags : 4;
+  U8 scanner_channel : 2;
+  U8 scan_direction_flag : 1;
+  U8 edge_of_flight_line : 1;
+  U8 classification;
+  U8 user_data;
+  I16 scan_angle;
+  U16 point_source_ID;
+};
+
+class LASwriteItemRaw_POINT14_LE : public LASwriteItemRaw
+{
+public:
+  LASwriteItemRaw_POINT14_LE(){};
+  inline BOOL write(const U8* item)
+  {
+    ((LAStempWritePoint14*)buffer)->x = ((LAStempWritePoint10*)item)->x;
+    ((LAStempWritePoint14*)buffer)->y = ((LAStempWritePoint10*)item)->y;
+    ((LAStempWritePoint14*)buffer)->z = ((LAStempWritePoint10*)item)->z;
+    ((LAStempWritePoint14*)buffer)->intensity = ((LAStempWritePoint10*)item)->intensity;
+    ((LAStempWritePoint14*)buffer)->return_number = ((LAStempWritePoint10*)item)->return_number;
+    ((LAStempWritePoint14*)buffer)->number_of_returns_of_given_pulse = ((LAStempWritePoint10*)item)->number_of_returns_of_given_pulse;
+    ((LAStempWritePoint14*)buffer)->classification_flags = (((LAStempWritePoint10*)item)->classification >> 5);
+    ((LAStempWritePoint14*)buffer)->scanner_channel = 0;
+    ((LAStempWritePoint14*)buffer)->scan_direction_flag = ((LAStempWritePoint10*)item)->scan_direction_flag;
+    ((LAStempWritePoint14*)buffer)->edge_of_flight_line = ((LAStempWritePoint10*)item)->edge_of_flight_line;
+    ((LAStempWritePoint14*)buffer)->classification = ((LAStempWritePoint10*)item)->classification & 31;
+    ((LAStempWritePoint14*)buffer)->user_data = ((LAStempWritePoint10*)item)->user_data;
+    ((LAStempWritePoint14*)buffer)->scan_angle = I16_QUANTIZE(((LAStempWritePoint10*)item)->scan_angle_rank/0.006f);
+    ((LAStempWritePoint14*)buffer)->point_source_ID = ((LAStempWritePoint10*)item)->point_source_ID;
+    *((F64*)&buffer[22]) = *((F64*)&item[24]);
+    return outstream->putBytes(buffer, 30);
+  }
+private:
+  U8 buffer[30];
+};
+
+class LASwriteItemRaw_RGBNIR14_LE : public LASwriteItemRaw
+{
+public:
+  LASwriteItemRaw_RGBNIR14_LE(){}
+  inline BOOL write(const U8* item)
+  {
+    return outstream->putBytes(item, 8);
+  };
+};
+
+class LASwriteItemRaw_RGBNIR14_BE : public LASwriteItemRaw
+{
+public:
+  LASwriteItemRaw_RGBNIR14_BE(){}
+  inline BOOL write(const U8* item)
+  {
+    ENDIAN_SWAP_32(&item[ 0], &swapped[ 0]); // R
+    ENDIAN_SWAP_32(&item[ 2], &swapped[ 2]); // G
+    ENDIAN_SWAP_32(&item[ 4], &swapped[ 4]); // B
+    ENDIAN_SWAP_32(&item[ 6], &swapped[ 6]); // NIR
+    return outstream->putBytes(swapped, 8);
+  };
+private:
+  U8 swapped[8];
+};
+
 #endif
