cb9426027ed46df1f73eb0e6e6a63f1f95c49a35
Howard Butler <hobu.inc@gmail.com>
2014-03-19 13:48:43 -0500

    try a different theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'LASzip'
-copyright = u'2011, Martin Isenburg'
+copyright = u'2014, Martin Isenburg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'scrolls'
+html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
