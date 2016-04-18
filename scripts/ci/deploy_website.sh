a307035df507e261de319e5efcbb863ec7e5f230
Howard Butler <howard@hobu.co>
2016-04-18 13:58:13 -0500

    another deploy attempt

--- scripts/ci/deploy_website.sh
+++ scripts/ci/deploy_website.sh
@@ -6,4 +6,7 @@ if [[ ! -z $TRAVIS_BUILD_DIR ]]; then
 buildpath="$TRAVIS_BUILD_DIR"
 fi
 
-docker run -v $buildpath:/data -w /data/docs laszip/docs aws s3 sync /docs/html/ s3://www.laszip.org --acl public-read --profile laszip --secret_key=$AWS_SECRET --access_key=$AWS_KEY
+export AWS_ACCESS_KEY_ID="$AWS_KEY"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET"
+
+docker run -v $buildpath:/data -w /data/docs laszip/docs aws s3 sync /docs/html/ s3://www.laszip.org --acl public-read
