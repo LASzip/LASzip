8bd7534c8c705ce12fb4720a37c920a60513c76f
Howard Butler <howard@hobu.co>
2016-04-18 14:19:43 -0500

    another path issue3

--- scripts/ci/deploy_website.sh
+++ scripts/ci/deploy_website.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 echo "deploying docs for $TRAVIS_BUILD_DIR/docs"
-buildpath=`pwd`
-if [[ ! -z $TRAVIS_BUILD_DIR ]]; then
-buildpath="$TRAVIS_BUILD_DIR"
-fi
 
 export AWS_ACCESS_KEY_ID="$AWS_KEY"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET"
 
-docker run -v $buildpath:/data -w /data/docs laszip/docs aws s3 sync ./html/ s3://www.laszip.org --acl public-read
+docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs laszip/docs aws s3 sync ./build/html/ s3://www.laszip.org --acl public-read
