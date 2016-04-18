e8c00c3262bd1a7cb66140c666bd14853e9bdc25
Howard Butler <howard@hobu.co>
2016-04-18 14:40:56 -0500

    forward aws creds to docker container

--- scripts/ci/deploy_website.sh
+++ scripts/ci/deploy_website.sh
@@ -5,4 +5,4 @@ echo "deploying docs for $TRAVIS_BUILD_DIR/docs"
 export AWS_ACCESS_KEY_ID="$AWS_KEY"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET"
 
-docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs laszip/docs aws s3 sync ./build/html/ s3://www.laszip.org --acl public-read
+docker run -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET" -e "AWS_ACCESS_KEY_ID=$AWS_KEY" -v $TRAVIS_BUILD_DIR:/data -w /data/docs laszip/docs aws s3 sync ./build/html/ s3://www.laszip.org --acl public-read
