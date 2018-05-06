2d3212bbc4a604ed1d88a93abcc969c284b2927a
Howard Butler <howard@hobu.co>
2018-05-06 17:03:41 -0500

    echo some noise for ssh login

--- scripts/ci/add_deploy_key.sh
+++ scripts/ci/add_deploy_key.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ -n "$encrypted_de53628fc98a_key" ]; then
-    openssl aes-256-cbc -K $encrypted_de53628fc98a_key -iv $encrypted_de53628fc98a_iv -in scripts/ci/pdaldocs-private.key.enc -out scripts/ci/pdaldocs-private.key -d
-    cp scripts/ci/pdaldocs-private.key ~/.ssh/id_rsa
+#    openssl aes-256-cbc -K $encrypted_de53628fc98a_key -iv $encrypted_de53628fc98a_iv -in scripts/ci/pdaldocs-private.key.enc -out scripts/ci/pdaldocs-private.key -d
+
+    echo "decrypting SSH for github login"
+    openssl aes-256-cbc -K $encrypted_de53628fc98a_key -iv $encrypted_de53628fc98a_iv -in scripts/ci/pdaldocs-private.key.enc -out ~\/.ssh/pdal-docs/id_rsa -d
+
+#    cp scripts/ci/pdaldocs-private.key ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
     echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 fi;
